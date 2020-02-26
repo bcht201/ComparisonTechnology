ef6b26208c6dfd1084b31ee35eaff25027cbdb15
bcht201 <brianting1227@gmail.com>
2020-02-26 16:35:19 +0000

    changes

---
+++ src/components/Results.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+class Results extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          payload: null
+        }
+    }
+
+    componentDidMount = () =>{
+        axios.get('https://demo.staging.energyhelpline-aws.com/api/results/e5cd18d9-dfe0-4dd2-b0d7-ab6d00fce188')
+        .then(response =>{
+            console.log(response);
+            this.setState({ payload: response.data })
+        })
+    }
+
+    sorting = () =>{
+        let order = [];
+        // Go through each price of array item, compare with rest of the array and sort into price order with the index for that listing
+        // render results list based on this array of indexes sorted by price 
+    }
+    
+    render(){
+        if(this.state.payload === null){
+            return(
+                <h1>Loading...</h1>
+            )
+        }
+        else{
+            console.log("rendering:", this.state.payload.elecResults);
+            return(
+                <div class = "resultsContainer">
+                    <ul>{
+                        this.state.payload.elecResults.map(item => <li>{ item.name }</li>)
+                        }
+                    </ul>
+                </div>
+            )
+        }
+    }
+}
+
+export default Results
